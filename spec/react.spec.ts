ab67be97730cb688b2a1d3d142e5b71bcda10fbf
Timo Westkämper <timo.westkamper@nosto.com>
2024-12-18 14:53:12 +0200

    Prettify

--- spec/react.spec.ts
+++ spec/react.spec.ts
@@ -6,54 +6,54 @@ import { DefaultState } from "../src/utils/state"
 import { autocompleteSuite } from "./suites/autocomplete"
 
 interface WindowWithReact extends Window {
-    React?: typeof React
-    ReactDOM?: typeof ReactDOM
+  React?: typeof React
+  ReactDOM?: typeof ReactDOM
 }
 
 let reactRoot: ReactDOM.Root | undefined
 const w = window as unknown as WindowWithReact
 
 function libraryScript() {
-    const reactScript = document.createElement("script")
-    reactScript.src = "https://unpkg.com/react@18/umd/react.production.min.js"
-    document.body.appendChild(reactScript)
+  const reactScript = document.createElement("script")
+  reactScript.src = "https://unpkg.com/react@18/umd/react.production.min.js"
+  document.body.appendChild(reactScript)
 
-    const reactDomScript = document.createElement("script")
-    reactDomScript.src =
-        "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"
-    document.body.appendChild(reactDomScript)
+  const reactDomScript = document.createElement("script")
+  reactDomScript.src =
+    "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"
+  document.body.appendChild(reactDomScript)
 
-    const babelScript = document.createElement("script")
-    babelScript.src = "https://unpkg.com/babel-standalone@6/babel.min.js"
-    document.body.appendChild(babelScript)
+  const babelScript = document.createElement("script")
+  babelScript.src = "https://unpkg.com/babel-standalone@6/babel.min.js"
+  document.body.appendChild(babelScript)
 }
 
 function render(container: HTMLElement, state: DefaultState) {
-    if (!reactRoot) {
-        reactRoot = w.ReactDOM?.createRoot(container)
-    }
-    reactRoot?.render(
-        w.React?.createElement(Autocomplete, {
-            history: state.history,
-            response: {
-                products: {
-                    hits: state.response?.products?.hits ?? [],
-                },
-                keywords: {
-                    hits: state.response?.keywords?.hits ?? [],
-                },
-            },
-        })
-    )
+  if (!reactRoot) {
+    reactRoot = w.ReactDOM?.createRoot(container)
+  }
+  reactRoot?.render(
+    w.React?.createElement(Autocomplete, {
+      history: state.history,
+      response: {
+        products: {
+          hits: state.response?.products?.hits ?? [],
+        },
+        keywords: {
+          hits: state.response?.keywords?.hits ?? [],
+        },
+      },
+    })
+  )
 }
 
 describe("from react component", () => {
-    afterEach(() => {
-        reactRoot = undefined
-    })
+  afterEach(() => {
+    reactRoot = undefined
+  })
 
-    autocompleteSuite({
-        render: () => render,
-        libraryScript,
-    })
+  autocompleteSuite({
+    render: () => render,
+    libraryScript,
+  })
 })
--- src/utils/input.ts
+++ src/utils/input.ts
@@ -1,102 +1,102 @@
 import { findAll } from "./dom"
 
 type Callbacks = {
-    onSubmit?: (value: string) => void
-    onInput?: (value: string) => void
-    onFocus?: (value: string) => void
-    onBlur?: (value: string) => void
-    onKeyDown?: (value: string, key: string) => void
-    onClick?: (value: string) => void
+  onSubmit?: (value: string) => void
+  onInput?: (value: string) => void
+  onFocus?: (value: string) => void
+  onBlur?: (value: string) => void
+  onKeyDown?: (value: string, key: string) => void
+  onClick?: (value: string) => void
 }
 
 export function bindInput(
-    selector: string | HTMLInputElement,
-    callbacks: Callbacks
+  selector: string | HTMLInputElement,
+  callbacks: Callbacks
 ): {
-    destroy: () => void
+  destroy: () => void
 } {
-    const target =
-        selector instanceof HTMLInputElement
-            ? [selector]
-            : findAll(selector, HTMLInputElement)
-    const unbindCallbacks = target.flatMap(el => {
-        const cbs: Array<() => void> = []
+  const target =
+    selector instanceof HTMLInputElement
+      ? [selector]
+      : findAll(selector, HTMLInputElement)
+  const unbindCallbacks = target.flatMap(el => {
+    const cbs: Array<() => void> = []
 
-        if (callbacks.onSubmit) {
-            const onKeyDown = (event: KeyboardEvent) => {
-                callbacks.onKeyDown?.(el.value, event.key)
-                if (
-                    event.key === "ArrowDown" ||
-                    event.key === "ArrowUp" ||
-                    event.key === "Enter"
-                ) {
-                    event.preventDefault()
-                }
-            }
-
-            el.addEventListener("keydown", onKeyDown)
-            cbs.push(() => {
-                el.removeEventListener("keydown", onKeyDown)
-            })
-
-            const form = el.form
-
-            if (form) {
-                const onSubmit = (event: SubmitEvent) => {
-                    event.preventDefault()
-                    callbacks.onSubmit?.(el.value)
-                }
-                form.addEventListener("submit", onSubmit)
-                cbs.push(() => {
-                    form?.removeEventListener("submit", onSubmit)
-                })
-
-                const buttons = Array.from(form.querySelectorAll("[type=submit]"))
-                buttons.forEach(button => {
-                    const onClick = (event: Event) => {
-                        event.preventDefault()
-                        callbacks.onSubmit?.(el.value)
-                    }
-
-                    button.addEventListener("click", onClick)
-                    cbs.push(() => {
-                        button.removeEventListener("click", onClick)
-                    })
-                })
-            }
+    if (callbacks.onSubmit) {
+      const onKeyDown = (event: KeyboardEvent) => {
+        callbacks.onKeyDown?.(el.value, event.key)
+        if (
+          event.key === "ArrowDown" ||
+          event.key === "ArrowUp" ||
+          event.key === "Enter"
+        ) {
+          event.preventDefault()
         }
+      }
 
-        if (callbacks.onClick) {
-            const onClick = () => {
-                callbacks.onClick?.(el.value)
-            }
-            el.addEventListener("click", onClick)
+      el.addEventListener("keydown", onKeyDown)
+      cbs.push(() => {
+        el.removeEventListener("keydown", onKeyDown)
+      })
+
+      const form = el.form
+
+      if (form) {
+        const onSubmit = (event: SubmitEvent) => {
+          event.preventDefault()
+          callbacks.onSubmit?.(el.value)
         }
+        form.addEventListener("submit", onSubmit)
+        cbs.push(() => {
+          form?.removeEventListener("submit", onSubmit)
+        })
 
-        if (callbacks.onFocus) {
-            const onFocus = () => {
-                callbacks.onFocus?.(el.value)
-            }
-            el.addEventListener("focus", onFocus)
-        }
+        const buttons = Array.from(form.querySelectorAll("[type=submit]"))
+        buttons.forEach(button => {
+          const onClick = (event: Event) => {
+            event.preventDefault()
+            callbacks.onSubmit?.(el.value)
+          }
 
-        if (callbacks.onInput) {
-            const onInput = () => {
-                callbacks.onInput?.(el.value)
-            }
-
-            el.addEventListener("input", onInput)
-            cbs.push(() => {
-                el.removeEventListener("input", onInput)
-            })
-        }
-
-        return cbs
-    })
-
-    return {
-        destroy() {
-            unbindCallbacks.forEach(v => v())
-        },
+          button.addEventListener("click", onClick)
+          cbs.push(() => {
+            button.removeEventListener("click", onClick)
+          })
+        })
+      }
     }
+
+    if (callbacks.onClick) {
+      const onClick = () => {
+        callbacks.onClick?.(el.value)
+      }
+      el.addEventListener("click", onClick)
+    }
+
+    if (callbacks.onFocus) {
+      const onFocus = () => {
+        callbacks.onFocus?.(el.value)
+      }
+      el.addEventListener("focus", onFocus)
+    }
+
+    if (callbacks.onInput) {
+      const onInput = () => {
+        callbacks.onInput?.(el.value)
+      }
+
+      el.addEventListener("input", onInput)
+      cbs.push(() => {
+        el.removeEventListener("input", onInput)
+      })
+    }
+
+    return cbs
+  })
+
+  return {
+    destroy() {
+      unbindCallbacks.forEach(v => v())
+    },
+  }
 }
