bc7d7345229e34fe124cc9dbb64143ccd9bd5bb6
Timo Westkämper <timo.westkamper@nosto.com>
2025-04-01 15:41:23 +0300

    Fix types

--- dev/dummySearch.ts
+++ dev/dummySearch.ts
@@ -1,11 +1,13 @@
+import { SearchKeyword, SearchResult } from "@nosto/nosto-js/client"
+
 function dummyProduct(index: number) {
   return {
     id: String(index),
     name: "Product " + index,
     url: "/product-" + index,
     imageUrl: `https://picsum.photos/id/${index + 100}/300/300`,
-    price: (Math.random() * 100).toFixed(2),
-    listPrice: (Math.random() * 100 + 50).toFixed(2),
+    price: +(Math.random() * 100).toFixed(2),
+    listPrice: +(Math.random() * 100 + 50).toFixed(2),
     brand: "Brand A",
   }
 }
@@ -20,15 +22,17 @@ const api = {
           dummyProduct(3),
           dummyProduct(4),
         ],
+        total: 4,
       },
       keywords: {
         hits: [
           { keyword: "keyword1" },
           { keyword: "keyword2" },
           { keyword: "keyword3" },
-        ],
+        ] as SearchKeyword[],
+        total: 3
       }
-    }),
+    } as SearchResult),
   recordSearchClick: () => {},
   internal: {
     getSettings: () => {},
