d53bb9f9097bbb9b3d8a251fba04f23dda44de8a
Timo Westkämper <timo.westkamper@nosto.com>
2025-04-01 15:03:17 +0300

    Preparations for dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [
@@ -14,6 +14,9 @@ export default defineConfig({
       },
     }
   ],
+  server: {
+    port: 8080
+  },
   test: {
     include: ["./spec/**/*.spec.{ts,tsx}"],
     environment: "jsdom",
